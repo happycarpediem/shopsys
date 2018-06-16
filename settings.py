2a5806c11a99a64f398a04e78dea913d6f53afb6
happy <103098723@qq.com>
2018-06-16 02:46:12 -0700

    update mysql

--- settings.py
+++ settings.py
@@ -73,10 +73,22 @@ WSGI_APPLICATION = 'shopsys.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'shopsys',
+        'USER':"shopsys",
+        'PASSWORD':"123456",
+        'HOST':'127.0.0.1',
+        'POST':'3306',
+        'TEST':{}
     }
 }
 
